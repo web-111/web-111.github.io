c54bd425fa7ac60e6e903ddfd9dabb78dd5e6bdf
web <wweb820@gmail.com>
2020-09-17 23:14:44 +0300

    3

--- script.js
+++ script.js
@@ -6,24 +6,4 @@ $(".close, #backdrop").click(function(){
     $("#form, #backdrop").show();
   });
 
-  $('#form').submit(function(){
-    $("#form").hide();
-    $('#form_res').show();
-    $.ajax({  
-      type: 'POST',  
-      url: 'telegram.php', 
-      data: {data: source},
-      success: function(response) {
-        $("#form").serialize(), // отправляемые данные  
-        function(msg) { // получен ответ сервера  
-          // $('#my_form_email').hide('slow');
-              $('#form_res').append(msg);
-          }	
-        }
-        
-         	
-        
-        
-    );
-    return false;
-});
+  
