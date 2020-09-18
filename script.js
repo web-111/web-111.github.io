63ec598d9482ec3dc250a26cbf934c636ebd7e0d
web <wweb820@gmail.com>
2020-09-18 10:23:43 +0300

    5

--- script.js
+++ script.js
@@ -6,4 +6,18 @@ $(".close, #backdrop").click(function(){
     $("#form, #backdrop").show();
   });
 
-  
+
+  $('#form').submit(function(){
+    $("#form").hide();
+    $('#form_res').show();
+    $.post(
+        'http://id-dent.vymirs.com/wp-content/themes/id-dent/img/telegram.php', // адрес обработчика
+         $("#form").serialize(), // отправляемые данные  		
+
+  function(msg) { // получен ответ сервера  
+    // $('#my_form_email').hide('slow');
+        $('#form_res').append(msg);
+}
+  );
+  return false;
+});
